5a03deea5d7d8ecab715891a0a6651b27501ceae
baidala <stavr@bk.ru>
2015-12-23 19:10:22 +0200

    Create FirstButton.java

---
+++ FirstButton.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.com.itstepandroid11.baydala;
+
+/**
+ *
+ * @author student
+ */
+public class FirstButton implements Callback {
+    private int buttonId;
+    
+    FirstButton (int id) {
+        buttonId = id;
+    }
+
+    @Override
+    public void startButtonById(int id) {
+        if ( buttonId == id ) {
+            System.out.println("Click First");
+        }
+    }
+    
+    
+}
