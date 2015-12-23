1d74a98b1d65342d131bdb3140fa35c47dd2ae29
baidala <stavr@bk.ru>
2015-12-23 19:50:50 +0200

    Create SecondButton.java

---
+++ SecondButton.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.com.itstepandroid11.baidala;
+
+/**
+ *
+ * @author student
+ */
+public class SecondButton implements Callback {
+
+    @Override
+    public void startButtonById(int id) {
+        
+    }
+    
+    
+}
